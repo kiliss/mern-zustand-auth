1177dab6cf72a03a9cf26f1afb4d437e04b38bfa
kiliss <fabigus14@gmail.com>
2023-02-08 15:50:50 -0300

    Creacion de libs para axios y agregado de zustand

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,5 +1,9 @@
-import axios from 'axios';
+import axios from "../libs/axios";
 
 export const LoginRequest = async (email: string, password: string) => {
   return axios.post('http://localhost:3000/login', { email, password });
+}
+
+export const profileRequest = async () => {
+  return await axios.get('http://localhost:3000/profile');
 }
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import {LoginRequest} from "../api/auth"
+import {LoginRequest, profileRequest} from "../api/auth"
+import {useAuthStore} from "../store/auth"
 
 function LoginPage() {
+
+  const setToken = useAuthStore (state => state.setToken)
+  const setUser = useAuthStore (state => state.setProfile)
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const email = (e.currentTarget.elements[0] as HTMLInputElement).value
     const password = (e.currentTarget.elements[1] as HTMLInputElement).value
     const resLogin = await LoginRequest(email, password)
-    console.log(resLogin)
+    setToken(resLogin.data.token)
+    const resProfile = await profileRequest()
+    setUser(resProfile.data.profile)
+    
   }
 
 
